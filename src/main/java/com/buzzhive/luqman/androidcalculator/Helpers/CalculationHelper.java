8e6f0a7b59620de450508c1dfb6d1ae8146ccd4e
Luqman Mohammed <m.luqman077@gmail.com>
2019-03-10 23:49:46 +0530

    added calculation logic

--- src/main/java/com/buzzhive/luqman/androidcalculator/Helpers/CalculationHelper.java
+++ src/main/java/com/buzzhive/luqman/androidcalculator/Helpers/CalculationHelper.java
@@ -1,4 +1,96 @@
 package com.buzzhive.luqman.androidcalculator.Helpers;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class CalculationHelper {
+
+    //main calculate function. segregates the multiple operand expression. adds negative numbers if available. and calculates according to BODMAS
+    public static double calculate(String expString) {
+        double awn = 0.0;
+
+        ArrayList<String> toBeCal = addNegatives(segregateExpr(expString));
+        int noOfOperands = 0;
+        for(String s : toBeCal) {
+            if (s.compareTo("-") == 0|| s.compareTo("+") == 0 || s.compareTo("*") == 0 || s.compareTo("/") == 0)
+                noOfOperands++;
+        }
+        try {
+            for(int j = 0;j< (int)noOfOperands;j++) {
+                for (int i = 0; i < toBeCal.size(); i++) {
+                    double temp = 0.0;
+                    if (toBeCal.get(i).compareTo("/") == 0) {
+                        temp = Double.parseDouble(toBeCal.get(i - 1)) / Double.parseDouble(toBeCal.get(i + 1));
+                        toBeCal.set(i - 1, Double.toString(temp));
+                        toBeCal.remove(i);
+                        toBeCal.remove(i);
+                    } else if (toBeCal.get(i).compareTo("*") == 0) {
+                        temp = Double.parseDouble(toBeCal.get(i - 1)) * Double.parseDouble(toBeCal.get(i + 1));
+                        toBeCal.set(i - 1, Double.toString(temp));
+                        toBeCal.remove(i);
+                        toBeCal.remove(i);
+                    }
+                }
+                for (int i = 0; i < toBeCal.size(); i++) {
+                    double temp = 0.0;
+                    if (toBeCal.get(i).compareTo("+") == 0) {
+                        temp = Double.parseDouble(toBeCal.get(i - 1)) + Double.parseDouble(toBeCal.get(i + 1));
+                        toBeCal.set(i - 1, Double.toString(temp));
+                        toBeCal.remove(i);
+                        toBeCal.remove(i);
+                    } else if (toBeCal.get(i).compareTo("-") == 0) {
+                        temp = Double.parseDouble(toBeCal.get(i - 1)) - Double.parseDouble(toBeCal.get(i + 1));
+                        toBeCal.set(i - 1, Double.toString(temp));
+                        toBeCal.remove(i);
+                        toBeCal.remove(i);
+                    }
+                }
+            }
+        }catch (ArithmeticException e) {
+            Toast t = Toast.makeText(null,"Invalid Expression",Toast.LENGTH_LONG);
+            t.show();
+        }
+        awn = Double.parseDouble(toBeCal.get(0));
+        return awn;
+    }
+
+    //Add support to calculate negative numbers
+    private static ArrayList<String> addNegatives(ArrayList<String> segregatedExprList) {
+
+        if(segregatedExprList.get(0).compareTo("-") == 0) {
+            segregatedExprList.set(1,"-"+segregatedExprList.get(1));
+            segregatedExprList.remove(0);
+        }
+        for (int i = 0; i < segregatedExprList.size(); i++) {
+            String posString = segregatedExprList.get(i);
+
+            if ((posString.compareTo("/") == 0) || (posString.compareTo("*") == 0) || (posString.compareTo("+") == 0) || (posString.compareTo("-") == 0)) {
+                if (segregatedExprList.get(i + 1).compareTo("-") == 0) {
+                    String s = segregatedExprList.get(i + 2);
+                    s = "-" + s;
+                    segregatedExprList.set(i + 1, s);
+                    segregatedExprList.remove(i + 2);
+                }
+            }
+        }
+
+        return segregatedExprList;
+    }
+
+    //Segregates Multiple operand Expression by operand and return a Array list with individual numbers and operands
+    private static ArrayList<String> segregateExpr(String expString) {
+        ArrayList<String> expList = new ArrayList<>();
+        StringBuilder tempSB = new StringBuilder();
+        for (char posChar : expString.toCharArray())
+            if ((posChar == '/') || (posChar == '*') || (posChar == '+') || (posChar == '-')) {
+                if (tempSB.length() > 0) {
+                    expList.add(tempSB.toString());
+                    tempSB = new StringBuilder();
+                }
+                expList.add(Character.toString(posChar));
+            } else
+                tempSB.append(posChar);
+        expList.add(tempSB.toString());
+        return expList;
+    }
 }
--- src/main/java/com/buzzhive/luqman/androidcalculator/MainActivity.java
+++ src/main/java/com/buzzhive/luqman/androidcalculator/MainActivity.java
@@ -2,43 +2,147 @@ package com.buzzhive.luqman.androidcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.buzzhive.luqman.androidcalculator.Helpers.CalculationHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    //String Builder to hold current view Expression
     private static StringBuilder sb = new StringBuilder();
-    private TextView tvMain;
+    //TextViews
+    private TextView tvMain,tvAwn;
+    //Boolean to make sure no operands are used one after another
+    private static boolean operandLock = true;
+    //For negative numbers
+    private static int minusCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.tvMain = findViewById(R.id.tvMain);
+        this.tvAwn = findViewById(R.id.tvAwn);
 
-        findViewById(R.id.num1).setOnClickListener(new MainButtonOnClickListener("1"));
-        findViewById(R.id.num2).setOnClickListener(new MainButtonOnClickListener("2"));
-        findViewById(R.id.num3).setOnClickListener(new MainButtonOnClickListener("3"));
-        findViewById(R.id.num0).setOnClickListener(new MainButtonOnClickListener("0"));
-        findViewById(R.id.num4).setOnClickListener(new MainButtonOnClickListener("4"));
-        findViewById(R.id.num5).setOnClickListener(new MainButtonOnClickListener("5"));
-        findViewById(R.id.num6).setOnClickListener(new MainButtonOnClickListener("6"));
-        findViewById(R.id.num7).setOnClickListener(new MainButtonOnClickListener("7"));
-        findViewById(R.id.num8).setOnClickListener(new MainButtonOnClickListener("8"));
-        findViewById(R.id.num9).setOnClickListener(new MainButtonOnClickListener("9"));
-        findViewById(R.id.btnDecimal).setOnClickListener(new MainButtonOnClickListener("."));
-        findViewById(R.id.btnAdd).setOnClickListener(new MainButtonOnClickListener("+"));
-        findViewById(R.id.btnMinus).setOnClickListener(new MainButtonOnClickListener("-"));
-        findViewById(R.id.btnMultiply).setOnClickListener(new MainButtonOnClickListener("*"));
-        findViewById(R.id.btnDivide).setOnClickListener(new MainButtonOnClickListener("/"));
+        //Setting OnClickListeners for number buttons
+        findViewById(R.id.num1).setOnClickListener(new NumButtonOnClickListener("1"));
+        findViewById(R.id.num2).setOnClickListener(new NumButtonOnClickListener("2"));
+        findViewById(R.id.num3).setOnClickListener(new NumButtonOnClickListener("3"));
+        findViewById(R.id.num0).setOnClickListener(new NumButtonOnClickListener("0"));
+        findViewById(R.id.num4).setOnClickListener(new NumButtonOnClickListener("4"));
+        findViewById(R.id.num5).setOnClickListener(new NumButtonOnClickListener("5"));
+        findViewById(R.id.num6).setOnClickListener(new NumButtonOnClickListener("6"));
+        findViewById(R.id.num7).setOnClickListener(new NumButtonOnClickListener("7"));
+        findViewById(R.id.num8).setOnClickListener(new NumButtonOnClickListener("8"));
+        findViewById(R.id.num9).setOnClickListener(new NumButtonOnClickListener("9"));
+
+        //Setting OnClickListener for operands
+        findViewById(R.id.btnDivide).setOnClickListener(new OperandButtonClickListener("/"));
+        findViewById(R.id.btnMultiply).setOnClickListener(new OperandButtonClickListener("*"));
+        findViewById(R.id.btnAdd).setOnClickListener(new OperandButtonClickListener("+"));
+        findViewById(R.id.btnDecimal).setOnClickListener(new OperandButtonClickListener("."));
+
+        //OnClickListener for minus operand. Special listener to implement negative numbers
+        findViewById(R.id.btnMinus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sb.length() == 0 ) {
+                    sb.append("-");
+                    minusCount = 10;
+                }
+                else if(minusCount < 2) {
+                    minusCount++;
+                    sb.append("-");
+                }
+                if(minusCount >= 2)
+                    operandLock = true;
+                tvMain.setText(sb.toString());
+            }
+        });
+
+        //Calculates starts here
+        findViewById(R.id.btnEquals).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(operandLock){
+                    double awn = CalculationHelper.calculate(sb.toString());
+                    tvAwn.setText(Double.toString(awn));
+                }
+                else {
+                    Toast t = Toast.makeText(null,"Invalid Expression",Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
+
+        //Clears Everything in screen
+        findViewById(R.id.btnClearEverything).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sb = new StringBuilder();
+                tvMain.setText(sb.toString());
+                operandLock = true;
+                minusCount = 0;
+                tvAwn.setText("");
+            }
+        });
+
+        //Clears last character and check if its a valid expression
+        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sb.length() > 0){
+                    if(sb.charAt(sb.length()-1) == '-')
+                        minusCount--;
+                    sb.deleteCharAt(sb.length()-1);
+                    tvMain.setText(sb.toString());
+                    try {
+                        Double.parseDouble(sb.charAt(sb.length()-1)+"");
+                    }catch(NumberFormatException nfe) {
+                        operandLock = true;
+                    }
+                }
+                if(sb.length() == 0)
+                    operandLock = true;
+            }
+        });
+
 
     }
 
-    protected class MainButtonOnClickListener implements View.OnClickListener {
+    //Inner Class to Implement Common Operand OnClickListener
+    protected class OperandButtonClickListener implements View.OnClickListener {
+
+        private String operand;
+
+        OperandButtonClickListener(String operand) {
+            this.operand = operand;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (!operandLock) {
+                sb.append(operand);
+                operandLock = true;
+            } else if (sb.length() == 1 && sb.charAt(0) == '-') {
+                return;
+            }else {
+                if(sb.length() > 0)
+                    sb.setCharAt(sb.length()-1,operand.charAt(0));
+            }
+            tvMain.setText(sb.toString());
+        }
+    }
+
+    //Inner Class to Implement Common Number OnClickListener
+    protected class NumButtonOnClickListener implements View.OnClickListener {
 
         private String buttonOperand;
 
-        MainButtonOnClickListener(String operand) {
+        NumButtonOnClickListener(String operand) {
             this.buttonOperand = operand;
         }
 
@@ -46,6 +150,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             sb.append(this.buttonOperand);
             tvMain.setText(sb.toString());
+            operandLock = false;
+            minusCount = 0;
         }
     }
 }
