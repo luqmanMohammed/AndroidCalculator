13766bc8c8018e3a5102b788c0f6343ee364b3e6
Luqman Mohammed <m.luqman077@gmail.com>
2019-03-11 09:50:54 +0530

    added app icon

--- src/main/java/com/buzzhive/luqman/androidcalculator/MainActivity.java
+++ src/main/java/com/buzzhive/luqman/androidcalculator/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.rsz_1coding_contest);
         this.tvMain = findViewById(R.id.tvMain);
         this.tvAwn = findViewById(R.id.tvAwn);
 
